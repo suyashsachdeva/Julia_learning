90b323fd4e4c537ee443ccccf32833ad743d74ae
Suyash Sachdeva <77083945+suyashsachdeva@users.noreply.github.com>
2024-11-02 22:06:58 +0530

    Added exception handling in Julia

---
+++ exception.jl
@@ -0,0 +1,20 @@
+using Printf
+using Statistics
+
+print("Enter a number ")
+num1 = chomp(readline())
+
+print("Enter a number ")
+num2 = chomp(readline())
+
+
+try
+    val = (parse(Int32, num1)) / (parse(Int32, num2))
+    if (val == Inf)
+        error("Can't Divide by Zero")
+    else
+    println(val)
+    end
+catch e 
+    println(e)
+end
